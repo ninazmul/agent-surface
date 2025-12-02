1d3d0a10226f7a84bd50fa2532b5c7ab32cda2b1
ninazmul <nazmulsaw@gmail.com>
2025-12-02 10:38:06 +0600

    promotion card updated

--- app/(root)/components/PromotionCard.tsx
+++ app/(root)/components/PromotionCard.tsx
@@ -99,7 +99,7 @@ const PromotionCard = ({ promotion, isAdmin }: Props) => {
               {isAdmin && (
                 <a
                   href={`/promotions/${promotion._id.toString()}/update`}
-                  className="absolute top-3 right-3 p-1 text-xs font-semibold rounded-full bg-white text-black shadow"
+                  className="absolute top-3 right-3 p-3 text-xs font-semibold rounded-full bg-white text-black shadow"
                 >
                   <Pencil className="w-4 h-4" />
                 </a>
