5ed18edf234c1a246dac759abb241cd448b3d9d0
ninazmul <nazmulsaw@gmail.com>
2025-12-04 17:16:15 +0600

    quotation table ui updated

--- app/(root)/components/QuotationTable.tsx
+++ app/(root)/components/QuotationTable.tsx
@@ -422,7 +422,7 @@ const QuotationTable = ({ leads }: { leads: ICombinedItem[] }) => {
                     </TableCell>
 
                     {/* QuotationStatus */}
-                    <TableCell className="align-top">
+                    <TableCell className="align-top mt-2">
                       {lead.quotationStatus ? (
                         <span className="px-4 py-2 text-xs font-medium rounded-full bg-green-100 text-green-700 border">
                           Accepted
