805d73cb019c71de515f91a1ab6b546f1b82bc84
ninazmul <nazmulsaw@gmail.com>
2025-12-05 11:42:35 +0600

    service from ui updated

--- app/(root)/components/ServiceForm.tsx
+++ app/(root)/components/ServiceForm.tsx
@@ -88,12 +88,10 @@ const ServiceForm = ({ type, Service, ServiceId }: ServiceFormProps) => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-6 rounded-2xl bg-teal-50 dark:bg-gray-800 p-6 shadow-sm"
+        className="rounded-2xl bg-white dark:bg-gray-900 p-6 shadow-sm space-y-4"
       >
         <div className="space-y-2">
-          <h2 className="text-xl font-semibold text-teal-800 dark:text-gray-100">
-            Service Information
-          </h2>
+          <h2 className="text-xl font-semibold">Service Information</h2>
         </div>
         {/* Title */}
         <FormField
@@ -203,7 +201,7 @@ const ServiceForm = ({ type, Service, ServiceId }: ServiceFormProps) => {
           type="submit"
           size="lg"
           disabled={form.formState.isSubmitting}
-          className="w-full rounded-2xl"
+          className="w-full col-span-2 rounded-xl bg-black hover:bg-gray-700 dark:bg-gray-700 dark:hover:bg-gray-600 text-white flex items-center gap-1"
         >
           {form.formState.isSubmitting
             ? "Submitting..."
