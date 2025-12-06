3ca2618ec9c67aee56e063d950278771dff8a9ad
ninazmul <nazmulsaw@gmail.com>
2025-12-06 17:22:23 +0600

    message table updated

--- app/(root)/components/MessageTable.tsx
+++ app/(root)/components/MessageTable.tsx
@@ -15,11 +15,6 @@ import { Trash } from "lucide-react";
 import { IMessage } from "@/lib/database/models/message.model";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { getProfileByEmail } from "@/lib/actions/profile.actions";
 import { getAdminCountriesByEmail, isAdmin } from "@/lib/actions/admin.actions";
 import NewMessageForm from "./NewMessageForm";
@@ -46,6 +41,7 @@ const MessageTable = ({
     Record<string, { name?: string; logo?: string }>
   >({});
   const [sending, setSending] = useState(false);
+  const [selectedThread, setSelectedThread] = useState<IMessage | null>(null);
 
   // Fetch agency names (only missing emails)
   const fetchAgencyNames = useCallback(
@@ -203,7 +199,7 @@ const MessageTable = ({
         className="mb-4 w-full md:w-1/2 lg:w-1/3 rounded-2xl"
       />
 
-      <div className="grid grid-cols-5 gap-4">
+      <div className="flex w-full gap-4">
         <div className="col-span-1 overflow-x-auto scrollbar-hide bg-gray-100 rounded-2xl">
           <Table>
             <TableBody>
@@ -216,6 +212,7 @@ const MessageTable = ({
                 return (
                   <TableRow
                     key={message._id.toString()}
+                    onClick={() => setSelectedThread(message)}
                     className="hover:bg-purple-500 hover:text-white rounded-2xl border-none"
                   >
                     <TableCell className="flex items-center space-x-2">
@@ -239,115 +236,6 @@ const MessageTable = ({
                           </div>
                         )}
                       </div>
-                    </TableCell>
-                    <TableCell>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <Button
-                            variant="ghost"
-                            className="px-3 py-1.5 text-sm font-medium text-blue-500 hover:text-blue-600 border border-blue-500 hover:bg-blue-200 transition-all rounded-full rounded-br-none"
-                          >
-                            View Messages
-                          </Button>
-                        </PopoverTrigger>
-                        <PopoverContent className="max-w-md max-h-72 overflow-hidden text-sm">
-                          <div className="overflow-y-auto flex-1 space-y-3 pr-1">
-                            {message.messages.map((msg) => {
-                              const isAdminMsg = msg.senderRole === "admin";
-                              return (
-                                <div
-                                  key={msg._id.toString()}
-                                  className={`flex ${
-                                    isAdminMsg ? "justify-end" : "justify-start"
-                                  }`}
-                                >
-                                  <div
-                                    className={`group relative max-w-[75%] px-4 py-2 rounded-2xl shadow-sm text-sm ${
-                                      isAdminMsg
-                                        ? "bg-blue-500 text-white rounded-br-none"
-                                        : "bg-gray-200 text-black rounded-bl-none"
-                                    }`}
-                                  >
-                                    <p>{msg.text}</p>
-                                    <span className="text-xs text-gray-600 mt-1 block text-right">
-                                      {new Date(msg.timestamp).toLocaleString(
-                                        "en-GB"
-                                      )}
-                                    </span>
-
-                                    {role === "admin" && (
-                                      <button
-                                        onClick={() =>
-                                          handleDeleteSingleMessage(
-                                            message._id.toString(),
-                                            msg._id.toString()
-                                          )
-                                        }
-                                        className={`absolute -top-1 ${
-                                          isAdminMsg ? "-left-1" : "-right-1"
-                                        } bg-red-500 text-white rounded-full p-1 hover:bg-red-600 
-                                    transition opacity-0 group-hover:opacity-100`}
-                                      >
-                                        <Trash size={14} />
-                                      </button>
-                                    )}
-                                  </div>
-                                </div>
-                              );
-                            })}
-                          </div>
-
-                          <div className="flex items-center gap-2 pt-2 border-t">
-                            <Input
-                              value={
-                                activeUserEmail === message.userEmail
-                                  ? newMessageText
-                                  : ""
-                              }
-                              onChange={(e) => {
-                                setActiveUserEmail(message.userEmail);
-                                setNewMessageText(e.target.value);
-                              }}
-                              placeholder="Type a message..."
-                              className="flex-1"
-                              onKeyDown={async (e) =>
-                                e.key === "Enter" &&
-                                (await handleAppendMessage())
-                              }
-                            />
-                            <Button
-                              size="sm"
-                              onClick={handleAppendMessage}
-                              disabled={sending}
-                            >
-                              {sending ? (
-                                <svg
-                                  className="animate-spin h-4 w-4 text-white mx-auto"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                >
-                                  <circle
-                                    className="opacity-25"
-                                    cx="12"
-                                    cy="12"
-                                    r="10"
-                                    stroke="currentColor"
-                                    strokeWidth="4"
-                                  ></circle>
-                                  <path
-                                    className="opacity-75"
-                                    fill="currentColor"
-                                    d="M4 12a8 8 0 018-8v8z"
-                                  ></path>
-                                </svg>
-                              ) : (
-                                "Send"
-                              )}
-                            </Button>
-                          </div>
-                        </PopoverContent>
-                      </Popover>
                     </TableCell>
 
                     <TableCell className="flex items-center space-x-2">
@@ -378,8 +266,102 @@ const MessageTable = ({
             </TableBody>
           </Table>
         </div>
-        <div className="col-span-3 bg-gray-100 rounded-2xl">
-          message section
+        <div className="flex-1 bg-gray-100 rounded-2xl p-4 flex flex-col">
+          {!selectedThread ? (
+            <div className="h-full flex items-center justify-center text-gray-500">
+              Select a profile to view messages
+            </div>
+          ) : (
+            <>
+              <div className="flex items-center gap-3 mb-4 border-b pb-2">
+                {agencyProfiles[selectedThread.userEmail]?.logo && (
+                  <Image
+                    src={
+                      agencyProfiles[selectedThread.userEmail]?.logo ||
+                      "/assets/user.png"
+                    }
+                    alt="logo"
+                    width={48}
+                    height={48}
+                    className="rounded-full object-cover"
+                  />
+                )}
+                <div className="font-semibold text-lg">
+                  {agencyNames[selectedThread.userEmail] ||
+                    selectedThread.userEmail}
+                </div>
+              </div>
+
+              <div className="flex-1 overflow-y-auto space-y-3 pr-2">
+                {selectedThread.messages.map((msg) => {
+                  const isAdminMsg = msg.senderRole === "admin";
+                  return (
+                    <div
+                      key={msg._id.toString()}
+                      className={`flex ${
+                        isAdminMsg ? "justify-end" : "justify-start"
+                      }`}
+                    >
+                      <div
+                        className={`group relative max-w-[75%] px-4 py-2 rounded-2xl shadow-sm text-sm ${
+                          isAdminMsg
+                            ? "bg-blue-500 text-white rounded-br-none"
+                            : "bg-gray-200 text-black rounded-bl-none"
+                        }`}
+                      >
+                        <p>{msg.text}</p>
+                        <span className="text-xs opacity-70 block text-right">
+                          {new Date(msg.timestamp).toLocaleString("en-GB")}
+                        </span>
+
+                        {role === "admin" && (
+                          <button
+                            onClick={() =>
+                              handleDeleteSingleMessage(
+                                selectedThread._id.toString(),
+                                msg._id.toString()
+                              )
+                            }
+                            className={`absolute -top-1 ${
+                              isAdminMsg ? "-left-1" : "-right-1"
+                            } bg-red-500 text-white rounded-full p-1 hover:bg-red-600 
+                    transition opacity-0 group-hover:opacity-100`}
+                          >
+                            <Trash size={14} />
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div className="flex items-center gap-2 pt-2 border-t">
+                <Input
+                  value={
+                    activeUserEmail === selectedThread.userEmail
+                      ? newMessageText
+                      : ""
+                  }
+                  onChange={(e) => {
+                    setActiveUserEmail(selectedThread.userEmail);
+                    setNewMessageText(e.target.value);
+                  }}
+                  placeholder="Type a message..."
+                  onKeyDown={async (e) =>
+                    e.key === "Enter" && (await handleAppendMessage())
+                  }
+                />
+                <Button
+                  size="sm"
+                  onClick={handleAppendMessage}
+                  disabled={sending}
+                >
+                  {sending ? "..." : "Send"}
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
 
