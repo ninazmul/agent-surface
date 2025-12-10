86753abc56c0b377d30d4201b4ead86cc284ee3e
ninazmul <nazmulsaw@gmail.com>
2025-12-10 10:39:40 +0600

    help button updated

--- components/shared/HelpForm.tsx
+++ components/shared/HelpForm.tsx
@@ -60,7 +60,7 @@ export default function HelpModal() {
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button className="fixed bottom-6 right-6 z-50 bg-black text-white hover:bg-white hover:text-black border border-black dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white rounded-full w-12 h-12 shadow-lg flex items-center justify-center">
-          <HelpCircle size={24} />
+          <HelpCircle className="w-10 h-10" />
         </Button>
       </DialogTrigger>
 
