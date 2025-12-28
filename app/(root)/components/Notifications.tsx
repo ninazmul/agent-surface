d6615f03d638a2625b48aa3ee060055ad28e45e6
ninazmul <nazmulsaw@gmail.com>
2025-12-28 12:21:53 +0600

    notification updated

--- app/(root)/components/Notifications.tsx
+++ app/(root)/components/Notifications.tsx
@@ -173,7 +173,7 @@ export default function NotificationsDropdown() {
       >
         <Bell className="w-5 h-5" />
         {unreadCount > 0 && (
-          <span className="absolute top-1 right-1 w-2.5 h-2.5 bg-red-600 rounded-full" />
+          <span className="absolute top-1 right-1 w-2.5 h-2.5 bg-red-600 rounded-full border border-white" />
         )}
       </Button>
 
