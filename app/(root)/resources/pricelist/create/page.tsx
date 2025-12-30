987834e7f82a6c0974004a5dd5b43352d0ddd3cc
ninazmul <nazmulsaw@gmail.com>
2025-12-30 13:19:43 +0600

    resource price list updated

--- app/(root)/resources/pricelist/create/page.tsx
+++ app/(root)/resources/pricelist/create/page.tsx
@@ -1,9 +1,9 @@
-import StudentResourceForm from "@/app/(root)/components/StudentResourceForm";
+import ResourcePriceListForm from "@/app/(root)/components/ResourcePriceListForm";
 
 const CreateResourcePage = async () => {
   return (
     <section className="max-w-5xl mx-auto p-4">
-      <StudentResourceForm type="Create" />
+      <ResourcePriceListForm type="Create" />
     </section>
   );
 };
