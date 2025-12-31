2d6e18010c249f1f8652f857aea96744ff18b73a
ninazmul <nazmulsaw@gmail.com>
2025-12-31 20:45:02 +0600

    campaign table ui updated

--- app/(root)/components/CampaignSubmissionsTable.tsx
+++ app/(root)/components/CampaignSubmissionsTable.tsx
@@ -150,14 +150,12 @@ export default function CampaignSubmissionsTable({
                 </TableCell>
 
                 <TableCell className="text-right">
-                  <Button
-                    size="sm"
-                    variant="outline"
-                    className="rounded-full"
+                  <span
+                    className="px-4 py-2 text-xs font-medium rounded-full bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600"
                     onClick={() => setActiveSubmission(sub)}
                   >
                     View Details
-                  </Button>
+                  </span>
                 </TableCell>
               </TableRow>
             ))}
