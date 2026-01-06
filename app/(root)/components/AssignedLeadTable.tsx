f1a199de02baff77765d8baef9d8729fb1b1286b
ninazmul <nazmulsaw@gmail.com>
2026-01-06 13:38:10 +0600

    lead and assigned lead table date and social ui fixed

--- app/(root)/components/AssignedLeadTable.tsx
+++ app/(root)/components/AssignedLeadTable.tsx
@@ -783,7 +783,7 @@ const AssignedLeadTable = ({
 
                     {/* Date */}
                     <TableCell className="w-max align-top">
-                      <div className="flex flex-col text-sm w-32">
+                      <div className="flex flex-col text-sm w-40">
                         <span>
                           <strong>Created:</strong>{" "}
                           {lead.createdAt
