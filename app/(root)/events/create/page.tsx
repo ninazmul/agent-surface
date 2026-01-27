4dcd9596e5664e4a4b4b3b5d30d45a72be4a6d8c
ninazmul <nazmulsaw@gmail.com>
2026-01-27 12:15:13 +0600

    event calender function updated 2

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,9 +1,12 @@
+import { getAllProfiles } from "@/lib/actions";
 import EventCalendarForm from "../../components/EventCalenderForm";
 
 const CreateleadPage = async () => {
+  const agencies = await getAllProfiles();
+
   return (
     <section className="max-w-5xl mx-auto p-4">
-      <EventCalendarForm type="Create" />
+      <EventCalendarForm type="Create" agencies={agencies} />
     </section>
   );
 };
